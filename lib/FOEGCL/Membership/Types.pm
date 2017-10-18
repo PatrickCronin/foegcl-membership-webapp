546888a5b70d1b05c1cd8bddbf47ef0b5bd4b626
Patrick Cronin <patrick@cronin-tech.com>
2017-10-17 22:26:29 -0400

    Create a Types library

---
+++ lib/FOEGCL/Membership/Types.pm
@@ -0,0 +1,16 @@
+package FOEGCL::Membership::Types;
+
+use FOEGCL::Membership::perlbase;
+
+use base 'MooseX::Types::Combine';
+
+__PACKAGE__->provide_types_from(
+    qw(
+        MooseX::Types::Common::String
+        MooseX::Types::Moose
+        MooseX::Types::Path::Tiny
+        MooseX::Types::Perl
+    )
+);
+
+1;
