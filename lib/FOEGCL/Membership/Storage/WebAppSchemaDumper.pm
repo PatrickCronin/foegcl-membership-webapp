ca93af13fdc809c71f441b4964b015f05b673f77
Patrick Cronin <patrick@cronin-tech.com>
2018-02-23 21:25:55 -0500

    Be explicit about which table in which to rename a relationship

--- lib/FOEGCL/Membership/Storage/WebAppSchemaDumper.pm
+++ lib/FOEGCL/Membership/Storage/WebAppSchemaDumper.pm
@@ -11,17 +11,15 @@ sub _build_db_config ( $self, @ ) {
 }
 
 sub _build_loader_options ( $self, @ ) {
-    return { db_schema => 'public', };
-}
-
-around '_build_default_loader_options' => sub ( $orig, $self, @args ) {
     return {
-        $self->$orig(@args)->%*,
+        db_schema => 'public',
         rel_name_map => {
-            state_abbr => 'state',    # CityStateZip.state => State
+            CityStateZip => {
+                state_abbr => 'state',    # CityStateZip.state => State
+            }
         },
     };
-};
+}
 
 __PACKAGE__->meta->make_immutable;
 
