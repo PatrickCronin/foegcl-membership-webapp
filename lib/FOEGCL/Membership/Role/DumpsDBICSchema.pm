7cabcdc11de788d671fedb05a74abb0cab940ddc
Patrick Cronin <patrick@cronin-tech.com>
2018-02-23 21:23:55 -0500

    Add documentation for Dumper command line args

--- lib/FOEGCL/Membership/Role/DumpsDBICSchema.pm
+++ lib/FOEGCL/Membership/Role/DumpsDBICSchema.pm
@@ -4,22 +4,33 @@ package FOEGCL::Membership::Role::DumpsDBICSchema;
 
 use FOEGCL::Membership::Moose::Role;
 
+use Const::Fast qw( const );
 use DBIx::Class::Schema::Loader qw( make_schema_at );
 use MooseX::Getopt::OptionTypeMap ();
 
 use FOEGCL::Membership::Types qw( Bool HashRef Path );
 use Path::Tiny qw( path );
 
+const my %ARG_CONSTANT => (
+    debug => 0,
+    dump_directory => './lib',
+    dry_run => 0,
+    overwrite_modifications => 0,
+    quiet => 1,
+);
+
 has debug => (
     is        => 'ro',
     isa       => Bool,
     predicate => '_has_debug',
+    documentation => "Whether or not debug messages should be displayed. Defaults to $ARG_CONSTANT{debug}.",
 );
 
 has dry_run => (
     is        => 'ro',
     isa       => Bool,
     predicate => '_has_dry_run',
+    documentation => qq{If true, don't actually write out the generated files. Defaults to $ARG_CONSTANT{dry_run}.},
 );
 
 has dump_directory => (
@@ -27,18 +38,21 @@ has dump_directory => (
     isa       => Path,
     coerce    => 1,
     predicate => '_has_dump_directory',
+    documentation => "The Schema root directory within which the Result and ResultSet classes will be dumped. Defaults to $ARG_CONSTANT{dump_directory}.",
 );
 
 has overwrite_modifications => (
     is        => 'ro',
     isa       => Bool,
     predicate => '_has_overwrite_modifications',
+    documentation => "Whether or not to update manually-modified files. Defaults to $ARG_CONSTANT{overwrite_modifications}.",
 );
 
 has quiet => (
     is        => 'ro',
     isa       => Bool,
     predicate => '_has_quiet',
+    documentation => "Whether or not to print status messages. Defaults to $ARG_CONSTANT{quiet}.",
 );
 
 has _db_config => (
@@ -73,6 +87,7 @@ requires qw( _build_db_config );
 
 with 'MooseX::Getopt::Dashes';
 
+# Should be overridden by consuming classes
 sub _build_loader_options ( $self, @ ) {
     return {};
 }
@@ -117,16 +132,17 @@ sub _build_default_loader_options ( $self, @ ) {
                 TimeStamp
                 )
         ],
-        debug                   => 0,
+        debug                   => $ARG_CONSTANT{debug},
         default_resultset_class => 'ResultSet',
-        dump_directory          => './lib',
+        dry_run                 => $ARG_CONSTANT{dry_run},
+        dump_directory          => $ARG_CONSTANT{dump_directory},
         filter_generated_code   => sub { return "#<<<\n$_[2]#>>>" },
         generate_pod            => 1,
         naming                  => 'current',
         only_autoclean          => 1,
-        overwrite_modifications => 0,
+        overwrite_modifications => $ARG_CONSTANT{overwrite_modifications},
         preserve_case           => 1,
-        quiet                   => 0,
+        quiet                   => $ARG_CONSTANT{quiet},
         skip_load_external      => 1,
         use_namespaces          => 1,
     };
