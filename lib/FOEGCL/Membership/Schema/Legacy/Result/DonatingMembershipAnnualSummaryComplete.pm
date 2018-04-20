18a4fd8611eb21aa3992e19b7ca048073f3a8365
Patrick Cronin <patrick@cronin-tech.com>
2018-04-19 22:35:47 -0400

    Update the Legacy Schema

--- lib/FOEGCL/Membership/Schema/Legacy/Result/DonatingMembershipAnnualSummaryComplete.pm
+++ lib/FOEGCL/Membership/Schema/Legacy/Result/DonatingMembershipAnnualSummaryComplete.pm
@@ -94,9 +94,9 @@ __PACKAGE__->table("DonatingMembershipAnnualSummaryComplete");
   is_nullable: 1
   original: {data_type => "currency"}
 
-=head2 Donation Total
+=head2 Count of Donations
 
-  accessor: 'donation_total'
+  accessor: 'count_of_donations'
   data_type: 'double precision'
   is_nullable: 1
   original: {data_type => "double"}
@@ -121,9 +121,9 @@ __PACKAGE__->add_columns(
     is_nullable => 1,
     original    => { data_type => "currency" },
   },
-  "Donation Total",
+  "Count of Donations",
   {
-    accessor    => "donation_total",
+    accessor    => "count_of_donations",
     data_type   => "double precision",
     is_nullable => 1,
     original    => { data_type => "double" },
@@ -131,8 +131,8 @@ __PACKAGE__->add_columns(
 );
 #>>>
 
-# Created by DBIx::Class::Schema::Loader v0.07046 @ 2018-02-24 23:46:11
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:E5xGBVbCS9G13gs5gl7xAA
+# Created by DBIx::Class::Schema::Loader v0.07046 @ 2018-04-19 22:30:44
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:wh4ZM9jkGu8xb4713EVCHQ
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
