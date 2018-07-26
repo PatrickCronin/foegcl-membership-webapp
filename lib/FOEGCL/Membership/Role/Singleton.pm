e6b0cea4938fbc17fd7e471545d58aa062865cbe
Patrick Cronin <patrick@cronin-tech.com>
2018-07-25 22:21:47 -0400

    Hash names should be singular

--- lib/FOEGCL/Membership/Role/Singleton.pm
+++ lib/FOEGCL/Membership/Role/Singleton.pm
@@ -2,21 +2,19 @@ package FOEGCL::Membership::Role::Singleton;
 
 use FOEGCL::Membership::Moose::Role;
 
-my %singletons;
+my %singleton;
 
 sub instance ( $class, @args ) {
-
-    # warn "Returned singleton instance for $class";
     die q{Can't get an instance from an instance!} if ref $class;
 
-    return $singletons{$class} //= $class->new(@args);
+    return $singleton{$class} //= $class->new(@args);
 }
 
 around 'new' => sub ( $orig, $self, @args ) {
     die q{Can't get a new instance of }
         . ( ref $self )
         . q{ -- instance already exists! Use `instance` instead of `new`.}
-        if $singletons{ ref $self };
+        if $singleton{ ref $self };
 
     return $self->$orig(@args);
 };
