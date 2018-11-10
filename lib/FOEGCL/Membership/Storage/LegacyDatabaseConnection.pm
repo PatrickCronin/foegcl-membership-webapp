0bd1bd31cde2bf2198c2f63f4e0857957a54c607
Patrick Cronin <pcronin@maxmind.com>
2018-11-10 00:32:14 -0500

    Fixed schema builder; tidied class

--- lib/FOEGCL/Membership/Storage/LegacyDatabaseConnection.pm
+++ lib/FOEGCL/Membership/Storage/LegacyDatabaseConnection.pm
@@ -7,35 +7,37 @@ use FOEGCL::Membership::Moose;
 use Const::Fast 'const';
 use FOEGCL::Membership::Types
     qw( HashRef Maybe NonEmptySimpleStr PortNumber );
+use Module::Runtime 'require_module';
 
 has db_config => (
-    is      => 'ro',
-    does    => 'ConfiguresDatabaseConnection',
+    is       => 'ro',
+    does     => 'ConfiguresDatabaseConnection',
     required => 1,
 );
 
 has schema => (
     init_arg => undef,
-    is      => 'ro',
-    isa     => 'DBIx::Class::Schema',
-    lazy    => 1,
-    builder => '_build_schema',
-    clearer => '_clear_schema',
+    is       => 'ro',
+    isa      => 'DBIx::Class::Schema',
+    lazy     => 1,
+    builder  => '_build_schema',
+    clearer  => '_clear_schema',
 );
 
 has dbh => (
     init_arg => undef,
-    is      => 'ro',
-    isa     => 'DBI::db',
-    lazy    => 1,
-    builder => '_build_dbh',
-    clearer => '_clear_dbh',
+    is       => 'ro',
+    isa      => 'DBI::db',
+    lazy     => 1,
+    builder  => '_build_dbh',
+    clearer  => '_clear_dbh',
 );
 
 sub _build_schema ( $self, @ ) {
-    return $SCHEMA_CLASS_NAME->connect(
-        $self->db_config->connect_info->@*
-    );
+    my $class = $self->db_config->schema_class_name;
+    require_module $class;
+
+    return $class->connect( $self->db_config->connect_info->@* );
 }
 
 sub _build_dbh ( $self, @ ) {
