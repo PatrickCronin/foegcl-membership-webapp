45bdf5a65d26d8ed61c693f23605df40a0d9d842
Patrick Cronin <patrick@cronin-tech.com>
2018-11-18 19:59:32 -0500

    Use a different import mechanism

--- lib/FOEGCL/Membership/Moose/Role.pm
+++ lib/FOEGCL/Membership/Moose/Role.pm
@@ -4,26 +4,28 @@ package FOEGCL::Membership::Moose::Role;
 
 use FOEGCL::Membership::perlbase;
 
-use Import::Into;
 use Moose::Exporter;
 use Moose::Role                    ();
 use MooseX::SemiAffordanceAccessor ();
 use namespace::autoclean           ();
 
-my ($import) = Moose::Exporter->setup_import_methods(
-    install => [ 'unimport', 'init_meta' ],
-    also    => ['Moose::Role'],
+my ( undef, undef, $init_meta ) = Moose::Exporter->setup_import_methods(
+    install => [qw( import unimport init_meta )],
+    also    => [
+        qw(
+            Moose::Role
+            MooseX::SemiAffordanceAccessor
+            ),
+    ],
 );
 
-sub import {
-    my $for_role = caller();
+sub init_meta {
+    namespace::autoclean->import::into(2);
+    FOEGCL::Membership::perlbase->import::into(2);
 
-    $import->( undef, { into => $for_role } );
-    MooseX::SemiAffordanceAccessor->import( { into => $for_role } );
-
-    my $caller_level = 1;
-    FOEGCL::Membership::perlbase->import::into($caller_level);
-    namespace::autoclean->import::into($caller_level);
+    goto \&$init_meta
+        if !!$init_meta;
+    return;
 }
 
 1;
